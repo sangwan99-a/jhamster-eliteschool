b0d28cdebf8027620466614eab4812e458604901
sangwan99-a <sahilsangwan2001@gmail.com>
2025-03-08 20:08:09 -0800

    Deleting the test files

--- src/main/java/com/elite/school/service/dto/AdminUserDTO.java
+++ src/main/java/com/elite/school/service/dto/AdminUserDTO.java
@@ -29,6 +29,9 @@ public class AdminUserDTO implements Serializable {
     @Size(max = 50)
     private String lastName;
 
+    @Size(max = 50)
+    private String address;
+
     @Email
     @Size(min = 5, max = 254)
     private String email;
